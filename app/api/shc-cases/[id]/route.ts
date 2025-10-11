3b097409c6bfb729562f29417f8969631095b4a9
osqazi <107077552+osqazi@users.noreply.github.com>
2025-10-12 00:23:53 +0500

    update

--- app/api/shc-cases/[id]/route.ts
+++ app/api/shc-cases/[id]/route.ts
@@ -8,14 +8,13 @@ import * as cheerio from 'cheerio';
 //   const { id } = (await context.params);        // ✅ no await
 //   const caseId = Number(id);
 
-export async function GET(request: Request, context: any) {
-  // Handle both new (Promise) and old (sync) params versions
-  const params = context.params instanceof Promise
-    ? await context.params
-    : context.params;
+export async function GET(
+  request: Request,
+  context: any
+) {
+  const id = Number(context.params.id);
+   const caseId = id;
 
-  const id = Number(params.id);
-  const caseId = id;
 
   
   const url = `https://cases.shc.gov.pk/khi/web/index.php?r=cases%2Fview&id=${caseId}`;
