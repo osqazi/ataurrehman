c0c7d6d8899976a4390787f49c826599a0adf220
osqazi <107077552+osqazi@users.noreply.github.com>
2025-10-20 14:19:56 +0500

    update

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,21 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-   experimental: {
-    // Add any experimental features you want to enable
-  },
-  images: {
-    formats: ['image/avif', 'image/webp'],
-  },
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//    experimental: {
+//     // Add any experimental features you want to enable
+//   },
+//   images: {
+//     formats: ['image/avif', 'image/webp'],
+//   },
+// };
+
+// export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
